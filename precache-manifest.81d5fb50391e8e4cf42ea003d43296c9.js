b15f7bfe3874119c14f074bf531a7942f080d618
Anastasiia Sereda <nastiasereda.1994@gmail.com>
2020-11-06 22:32:44 +0200

    Updates

---
+++ precache-manifest.81d5fb50391e8e4cf42ea003d43296c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee625c5b08b0a8a94b2e45ded4259a7",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "4951059c3a38b40d0a27",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.95c730a0.chunk.css"
+  },
+  {
+    "revision": "9605b9d092068d2af1a6",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.d36ccddf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.d36ccddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4951059c3a38b40d0a27",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.7c13f768.chunk.js"
+  },
+  {
+    "revision": "f9c431807a28971805a1",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.f5d5d69b.js"
+  }
+]);
